e59c7ca1b91334f3130a4cbb675c02d0c268de49
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2023-12-30 11:19:27 +0100

    added aos to about section

--- src/components/Home/AboutSection.jsx
+++ src/components/Home/AboutSection.jsx
@@ -4,6 +4,7 @@ import { FaHandsHelping } from "react-icons/fa";
 import { LiaHandsHelpingSolid } from "react-icons/lia";
 import { RiServiceFill } from "react-icons/ri";
 import { GrBusinessService } from "react-icons/gr";
+import { Link } from "react-router-dom";
 
 const AboutComponent = () => {
   return (
@@ -18,8 +19,8 @@ const AboutComponent = () => {
           data-aos="fade-up"
           className="row d-flex justify-content-center my-3"
         >
-          <div className="col-lg-9 text-center">
-            <p className="lead">
+          <div className="col-lg-8 text-center">
+            <p className="text-dark">
               Omojolagbe Geriatrics Care Foundation was established in 2018 in
               Ibadan, Oyo State Nigeria and fully registered by The Corporate
               Affairs Commission. It is a reputable organization working in the
@@ -27,6 +28,12 @@ const AboutComponent = () => {
               We have impacted the lives of over 1000 elderly people in Ibadan
               and Nigeria in general.
             </p>
+            <Link
+              to={"/about"}
+              className="btn btn-outline-primary rounded-pill btn-sm"
+            >
+              Read More
+            </Link>
           </div>
         </div>
 
@@ -45,57 +52,59 @@ const AboutComponent = () => {
           </div>
         </div>
 
-        <div className="row">
-          <div
-            data-aos="fade-up"
-            className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
-          >
-            <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
-              <FaHandsHelping className="bg-dark rounded fs-1 text-warning" />
-              <p className="fw-normal mt-1">
-                We provide high quality health care services that are efficient
-                and supportive to the elderly persons.
-              </p>
+        <div className="row d-flex justify-content-center">
+          <div className="col-lg-10 row">
+            <div
+              data-aos="fade-up"
+              className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
+            >
+              <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
+                <FaHandsHelping className="bg-dark rounded fs-1 text-warning" />
+                <p className="mt-1">
+                  We provide high quality health care services that are
+                  efficient and supportive to the elderly persons.
+                </p>
+              </div>
             </div>
-          </div>
-          <div className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch">
             <div
               data-aos="fade-up"
               data-aos-delay="50"
-              className="about__text text-center p-1 shadow rounded border border-1 border-dark"
+              className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
             >
-              <LiaHandsHelpingSolid className="bg-dark rounded fs-1 text-warning" />
-              <p className="fw-normal mt-1">
-                We use all our efforts in taking care of the aged so as to
-                regain their independence and active ageing.
-              </p>
+              <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
+                <LiaHandsHelpingSolid className="bg-dark rounded fs-1 text-warning" />
+                <p className="fw-normal mt-1">
+                  We use all our efforts in taking care of the aged so as to
+                  regain their independence and active ageing.
+                </p>
+              </div>
             </div>
-          </div>
-          <div
-            data-aos="fade-up"
-            data-aos-delay="100"
-            className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
-          >
-            <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
-              <RiServiceFill className="bg-dark rounded fs-1 text-warning" />
-              <p className="fw-normal mt-1">
-                We conduct empowerment program for the vulnerable among the
-                elderly.
-              </p>
+            <div
+              data-aos="fade-up"
+              data-aos-delay="100"
+              className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
+            >
+              <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
+                <RiServiceFill className="bg-dark rounded fs-1 text-warning" />
+                <p className="fw-normal mt-1">
+                  We conduct empowerment program for the vulnerable among the
+                  elderly.
+                </p>
+              </div>
             </div>
-          </div>
-          <div
-            data-aos="fade-up"
-            data-aos-delay="150"
-            className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
-          >
-            <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
-              <GrBusinessService className="bg-dark rounded fs-1 text-warning" />
-              <p className="fw-normal mt-1">
-                We collaborate with relevant organizations within and outside
-                the country also to provide support and structure for the
-                elderly.
-              </p>
+            <div
+              data-aos="fade-up"
+              data-aos-delay="150"
+              className="col-lg-3 col-md-6 my-2 d-flex align-items-stretch"
+            >
+              <div className="about__text text-center p-1 shadow rounded border border-1 border-dark">
+                <GrBusinessService className="bg-dark rounded fs-1 text-warning" />
+                <p className="fw-normal mt-1">
+                  We collaborate with relevant organizations within and outside
+                  the country also to provide support and structure for the
+                  elderly.
+                </p>
+              </div>
             </div>
           </div>
         </div>
