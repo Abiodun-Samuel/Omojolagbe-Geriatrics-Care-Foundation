a62751910ab7915c87ccec57a20a12c7dd401281
Abiodun-Samuel <abiodunsamyemi@gmail.com>
2024-01-01 01:24:12 +0100

    service section

--- src/components/Home/ServiceSection.jsx
+++ src/components/Home/ServiceSection.jsx
@@ -1,18 +1,32 @@
 import imageOne from "../../assets/images/img/image1.png";
 import imageTwo from "../../assets/images/img/image2.png";
+import arrow from "../../assets/images/img/arrow-right.png";
 import SectionHeader from "../common/SectionHeader";
 const ServiceSection = () => {
   return (
     <section id="service__section">
-      <div className="container-sm">
+      <div className="container">
         <div className="row ">
-          <div data-aos="fade-up" className="col-lg-6 mb-1">
+          <div
+            data-aos="fade-up"
+            className="col-lg-6 mb-3 d-flex align-items-center"
+          >
             <img src={imageOne} alt="service image" className="img-fluid" />
+            <img
+              style={{
+                transform: "scaleX(-1)",
+                webkitTransform: "scaleX(-1)",
+              }}
+              className="d-none d-lg-block"
+              src={arrow}
+              alt="service image"
+              height={150}
+            />
           </div>
           <div
             data-aos="fade-up"
             data-aos-delay="50"
-            className="col-lg-6 d-flex align-items-center mb-1"
+            className="col-lg-6 d-flex align-items-center mb-3"
           >
             <div>
               <SectionHeader>
@@ -20,14 +34,14 @@ const ServiceSection = () => {
                   Personal <span className="text-warning">Care</span>
                 </h2>
               </SectionHeader>
-              <p className="mt-2 text-center lead">
+              <p className="mt-2  lead">
                 At Omojolagbe Geriatrics Care Foundation, we understand the
                 unique needs and challenges that come with aging. Our mission is
                 to provide compassionate and reliable personal care services
                 tailored specifically for seniors, ensuring a life of dignity,
                 comfort, and independence.
               </p>
-              <p className="text-center lead">
+              <p className=" lead">
                 We provide a Comprehensive Personal Assistance: From bathing,
                 grooming, and dressing to medication reminders and mobility
                 assistance, our caregivers are trained to offer support with the
@@ -39,7 +53,7 @@ const ServiceSection = () => {
           <div
             data-aos="fade-up"
             data-aos-delay="100"
-            className="col-lg-6 d-flex align-items-center mb-1"
+            className="col-lg-6 d-flex align-items-center mb-3"
           >
             <div>
               <SectionHeader>
@@ -47,14 +61,14 @@ const ServiceSection = () => {
                   Quality Of <span className="text-warning">Life</span>
                 </h2>
               </SectionHeader>
-              <p className="mt-2 text-center lead">
+              <p className="mt-2  lead">
                 At Omojolagbe Geriatrics Care Foundation, we`re dedicated to
                 providing more than just care; were committed to enhancing
                 qualify of lives. Our trained caregivers engage seniors in
                 meaningful activities, conversations, and outings to foster
                 emotional well-being and social interactions.
               </p>
-              <p className="mt-2 text-center lead">
+              <p className="mt-2  lead">
                 Life is meant to be lived fully at every age. Our center is a
                 vibrant hub of activities like arts, crafts and games. There`s
                 always something enriching happening here.
@@ -65,9 +79,15 @@ const ServiceSection = () => {
           <div
             data-aos="fade-up"
             data-aos-delay="150"
-            className="col-lg-6 mb-1 text-center "
+            className="col-lg-6 mb-3 text-end d-flex align-items-center"
           >
-            <img src={imageTwo} alt="service image" className="img-fluid" />
+            <img
+              className="d-none d-lg-block"
+              src={arrow}
+              alt="service image"
+              height={150}
+            />
+            <img src={imageTwo} alt="service image" className="img-fluid " />
           </div>
         </div>
       </div>
